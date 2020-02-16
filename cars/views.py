870b6c4a9b9705f9016720363590ec0c10d4fd6a
PC <fdhfdh>
2020-02-16 17:26:56 +0200

    addnig my car site

--- cars/views.py
+++ cars/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Car
+from .forms import CarForm
+from django.core.paginator import Paginator
 
 def car_list(request):
 	cars = Car.objects.all()
+	paginator = Paginator(cars, 3)
+	page_number = request.GET.get('page')
+	page_obj = paginator.get_page(page_number)
+
 	context = {
-		"cars": cars,
-	}
+        "cars":page_obj,
+    }
 	return render(request, 'car_list.html', context)
 
 
@@ -18,15 +24,33 @@ def car_detail(request, car_id):
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
+	form = CarForm()
+	if request.method == "POST":
+		form = CarForm(request.POST,request.FILES)
+		if form.is_valid():
+			form.save()
+			return redirect('car-list')
+	context = {
+        "form":form,
+    }
+	return render(request, 'create.html', context)
 
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
-
+	car = Car.objects.get(id=car_id)
+	form = CarForm(instance=car)
+	if request.method == "POST":
+		form = CarForm(request.POST, instance=car )
+		if form.is_valid():
+			form.save()
+			return redirect('car-list')
+	context = {
+        "car": car,
+        "form":form,
+    }
+	return render(request, 'update.html', context)
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+	car = Car.objects.get(id=car_id)
+	car.delete()
+	return redirect('car-list')
